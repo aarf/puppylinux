35c931d891e178ca543fabbfcbf1e18fbebacefc
aarf <aarf@yopmail.com>
2017-09-18 15:15:42 +0700

    Add files via upload

---
+++ startslimjet.sh
@@ -0,0 +1,7 @@
+cd /mnt/sda11/slimjet_amd64-15.1.3.0.tar.xz.extracted/slimjet
+
+#./flashpeak-slimjet --user-data-dir /mnt/sda11/slimjetdata 
+./flashpeak-slimjet --user-data-dir /mnt/sda11/slimjetdata --no-sandbox
+
+#su spot 
+#./flashpeak-slimjet --user-data-dir /mnt/sda11/slimjetdata 
