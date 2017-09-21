8ddfecf39d07bac00cc6ead6a45733acdb232193
aarf <aarf@yopmail.com>
2017-09-21 09:35:52 +0700

    Add files via upload

--- brightness-1.1.sh
+++ brightness-1.1.sh
@@ -1,5 +1,5 @@
  #!/bin/sh
  
  activeOutput=$(xrandr | grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
-
+#manually edit the last number to the brightness number you want 1 is max, 0 is lowest brightness
  xrandr --output $activeOutput --brightness .35000000000000000000
