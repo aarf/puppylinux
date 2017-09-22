26158b6c2ea5c98c3cd54a6d79b1278b288fdfdd
aarf <aarf@yopmail.com>
2017-09-23 02:42:47 +0700

    Add files via upload

---
+++ remove java symlinks-0.1.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+rm -f /usr/bin/java
+rm -f /usr/lib64/mozilla/plugins/libnpjp2.so
+rm -f /usr/bin/jcontrol
+rm -f /usr/bin/javaws
+rm -f /usr/bin/java_vm
+rm -f /usr/local/bin/java
+rm -f /usr/local/bin/jcontrol
+rm -f /usr/local/bin/javaws
+rm -f /usr/local/bin/java_vm
+rm -f /opt/java
