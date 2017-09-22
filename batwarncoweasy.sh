f355396ec76b4f0c88da70139917076961e8bfca
aarf <aarf@yopmail.com>
2017-09-23 02:24:20 +0700

    Add files via upload

---
+++ batwarncoweasy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sleep 1
+ #xpupsay --image /usr/share/xcowsay/cow_med.png -t 3 "woof! woof! Hello and welcome."
+
+xpupsay --image /usr/share/doc/easy/images/easy96.png -t 5 "Battery is getting low"
+
+
