5087aeabc45d9e43a5ae75a88102fd86ac8fd600
aarf <aarf@yopmail.com>
2017-09-24 16:37:46 +0700

    Add files via upload

---
+++ gogogofish.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ #go(){ echo fish; } ; go ; go ; go 
+ 
+ #go(){ /usr/local/bin/xmessage "fish";} ; go ; go ; go 
+ 
+ #go(){ /usr/local/bin/xmessage "${xclip -o}"} ; go ; go ; go 
+ 
+ #go(){ /usr/local/bin/xmessage<<{xclip -o}} ; go ; go ; go 
+ 
+ #/usr/local/bin/xmessage "fish"
+ 
+ go(){ yaf-splash -bg lightgreen -fg black  -fontsize  x-large -placement center -timeout 2 -text "fish";} ; go ; go ; go 
