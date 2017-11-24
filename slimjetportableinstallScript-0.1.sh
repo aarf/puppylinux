22b964fd1bd0316c29d9cbe512891b299724cb5c
aarf <aarf@yopmail.com>
2017-11-24 20:48:58 +0700

    Add files via upload

--- slimjetportableinstallScript-0.1.sh
+++ slimjetportableinstallScript-0.1.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 ##need internet connection
+#before starting need to rename the downloaded tar.xz eg slimjet_amd64-16.0.8.0.tar.xz
 #put this script and the downloaded slimjetXXX.tar.gz into the place where you want the portable install
 #need uextract to be installed
+#click startslimjet.sh after install to run 
 
 uextract slimjet*tar.xz
 
@@ -25,6 +27,7 @@ rm  flashpeak-slimjet
  echo "$cdline"
  #my gawd will you look at this nonsense the sed line below wont work if  / is in $cdline
  cdline=$(echo ${cdline//"/"/"\/"})
- #have tried just using pwd but it has issues with either desktop link or startup link (both to come in next editions of this script) for now do these manually if you want them
+ #have tried just using pwd but it has issues with either desktop link or startup link (both to come in next editions of this script) 
+ #for now do these manually if you want them
 sed -i "1s/.*/$cdline/" startslimjet.sh
 exit
