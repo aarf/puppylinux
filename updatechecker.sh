f52c0ec8f83c908f4f66422c8a2e89414e25171c
aarf <aarf@yopmail.com>
2018-01-21 00:31:22 +0700

    Add files via upload

--- updatechecker.sh
+++ updatechecker.sh
@@ -16,12 +16,22 @@ echo $name
 
  #wget  -4c $line -O fileurl.txt | md5sum fileurl.txt>>updatecheck_md5sum.txt
  
- wget  -4c $line  -O "$name" |  md5sum "$name">>updatecheck_md5sum.txt
-
+ wget  -4c $line  -O "$name" 
+md5sum "$name">>updatecheck_md5sum.txt
 done 
-diff updatecheck_md5sum.txt /root/.config/newpagechecker
-#mv updatecheck_md5sum.txt /root/.config/newpagechecker
+say=$(diff updatecheck_md5sum.txt /root/.config/newpagechecker/updatecheck_md5sum.txt)
+#diff lightofdawn.org-blog-viewCatFatdog64 /root/.config/newpagechecker/lightofdawn.org-blog-viewCatFatdog64
+
 
 #for new in $(cat updatecheck_md5sum.txt   ); do
 
 #done
+
+if [ "$say" = "$old" ]
+
+then true
+else xcowsay -t 0 --image /usr/share/icons/qpupster.png "$say"
+#mv lightofdawnmd5sum.txt  /root/.config/newpagechecker
+#mv updatecheck_md5sum.txt /root/.config/newpagechecker
+
+fi
