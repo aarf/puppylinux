2afa0686b9052ed316c771764121fa93801d05a6
aarf <aarf@yopmail.com>
2018-01-31 01:58:27 +0700

    Add files via upload

--- upper2lower.sh
+++ upper2lower.sh
@@ -3,9 +3,13 @@
  #cat output.txt
  xclip -o>>input_text.txt
 input_text=`cat /root/input_text.txt`
-
+output=""
 echo $input_text 
 tr '[:upper:]' '[:lower:]' < input_text.txt | xclip 
+output=$(tr '[:upper:]' '[:lower:]' < input_text.txt ) 
+echo $output
+xdotool sleep 0.3 type --clearmodifiers "$output"
+
 rm input_text.txt
 
  
