a8d2743e19f2bab5705e71a1c0abf49ef6c19552
aarf <aarf@yopmail.com>
2018-01-31 04:22:29 +0700

    Add files via upload

--- insertCodeMenubox.sh
+++ insertCodeMenubox.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-#DIALOG=${DIALOG=Xdialog}
-#tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/test$$
-#trap "rm -f $tempfile" 0 1 2 5 15
 
 codeselect=`Xdialog --stdout --clear --title "My  Choice" \
         --menubox "Hi, Choose  your choice:" 20 51 4 \
        "[img][/img]" "image" \
         "[url=][/url]" "url" \
        "[quote][/quote]" "quote"  \
-        "4" "four" \
+        "wget -4c " "wget" \
         "5"  "five" \
         "6"  "six" `  
         
-      #  choice=$codeselect
+
 echo code is $codeselect
 case "$codeselect" in
  "[img][/img]")
@@ -41,3 +38,35 @@ case $retval in
   255)
     echo "ESC pressed.";;
 esac
+xdotool getactivewindow
+#xdotool keydown key 1; sleep 1; xdotool keyup 1
+xdotool sleep 0.3 type --clearmodifiers "$codeselect"
+
+COUNTER=0
+         while [  $COUNTER -lt $NoLeft ]; do
+             xdotool   keydown key Left; sleep 0.3; xdotool   keyup Left ;
+             let COUNTER=COUNTER+1 
+         done
+
+
+xclip -selection primary -o>>/root/input_text.txt
+input_text=`cat /root/input_text.txt`
+rm /root/input_text.txt
+
+
+echo "You entered:$input_text"
+
+xdotool sleep 0.3 type --clearmodifiers "${input_text//-/-}"
+
+if  [ "$codeselect" = "[url=][/url]" ] 
+then xdotool   keydown key Right; sleep 0.3; xdotool   keyup Right ;
+fi
+if  [ "$codeselect" = "[img][/img]" ] 
+then 
+COUNTER=0
+         while [  $COUNTER -lt $NoRight ]; do
+             xdotool   keydown key Right; sleep 0.3; xdotool   keyup Right ;
+             let COUNTER=COUNTER+1 
+         done
+#xdotool   keydown key Right; sleep 0.3; xdotool   keyup Right ;
+fi
