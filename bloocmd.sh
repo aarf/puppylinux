c8605bf246bf23f14aa5a407a3d283e83821965c
aarf <aarf@yopmail.com>
2018-02-08 03:36:00 +0700

    Add files via upload

--- bloocmd.sh
+++ bloocmd.sh
@@ -5,15 +5,18 @@ cd /tmp
 #curl  "https://www.bloomberg.com/markets/commodities" --output bloocommod.txt 
   sed -n '/^$/!{s/<[^>]*>//g;p;}' < bloocommod.txt > bloocmdout.txt
   grep  -o 'bbl. [0-9]*.....[0-9]*' bloocmdout.txt > 2cow
-echo "Crude 2cow " >cmdout
-  numoil=$(tr "bbl.  [[:space:]]*" " " < 2cow )
-  echo troil  "$numoil" 
- numoil=$(grep -o  'bbl.  [[:space:]]*' < 2cow )
- 
-numoil=$(printf "%'.4f" $numoil)
-echo oil  "$numoil" 
+#echo "Crude 2cow " >cmdout
+  
 grep  -o 'MMBtu  [0-9]*.....[0-9]*' bloocmdout.txt >> 2cow
 grep  -o 't oz\. [0-9]*.....[0-9]*' bloocmdout.txt >> 2cow
+sed -i '1s/^/WTI Crude Oil USD\//' 2cow
+
+sed -i '2s/^/Brent Crude (ICE) USD\//' 2cow
+sed -i '3s/^/Natural Gas (Nymex)  USD\//' 2cow
+sed -i '4s/^/Gold USD\//' 2cow
+sed -i '5s/^/Gold Spot USD\//' 2cow
+sed -i '6s/^/Silver USD\//' 2cow
+sed -i '7s/^/Platinum USD\//' 2cow
 
  xcowsay -t 0 --image /mnt/sda11/slimjet_amd64-17.0.9.0.tar.xz.extracted/slimjet/product_logo_128.png <2cow 
  WTI Crude Oil 
