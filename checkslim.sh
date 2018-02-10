2547ceeda3dc519fca69781560a54bd8ec25fd68
aarf <aarf@yopmail.com>
2018-02-10 10:36:20 +0700

    Add files via upload

---
+++ checkslim.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd /tmp
+name="slimjet"
+line="https://www.slimjet.com/en/dlpage.php"
+#wget  -4c $line  -O "$name" 
+
+versionslim=$(grep  -o  'Stable V..................'  $name)
+versionslim=$(echo "$versionslim" | head -1 )
+oldverslim=$(cat /root/.config/newpagechecker/oldverslim)
+if [ "$versionslim" = "$oldverslim" ]
+then exit
+fi
+say="Current installed version of Slimjet is $oldversion, available for download now is $versionslim"
+xcowsay -t 0 --image /usr/share/icons/qpupster.png "$say"
+  Xdialog --yesno  "The current installed version will be updated in you click Yes"   5 100 ; yes=$?
+#update if yes
+if [ $yes = "0" ] 
+ then 
+ echo "$versionslim" >/root/.config/newpagechecker/oldverslim
+fi
