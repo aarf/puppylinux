9536d9b5704611659551af0f146b0ef916b31c5d
aarf <aarf@yopmail.com>
2018-02-24 15:08:39 +0700

    Add files via upload

--- tropicrisk.sh
+++ tropicrisk.sh
@@ -2,6 +2,11 @@
 sleep 60
 cd /tmp
 wget  -4c http://www.tropicalstormrisk.com/tracker/dynamic/main_.html
+connected=$(ifconfig wlan0 | grep -i 'inet' )
+if [ "$connected" = "" ] 
+then 
+exit
+fi
 
 noactive=$(grep -i 'no active' main*)
 if [ "$noactive" = "" ] 
