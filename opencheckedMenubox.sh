7fb7d75b5c6ed51813ea500a3aa2d6fcef7b54e4
aarf <aarf@yopmail.com>
2018-03-06 23:38:33 +0700

    Add files via upload

--- opencheckedMenubox.sh
+++ opencheckedMenubox.sh
@@ -3,14 +3,14 @@
 
 codeselect=`Xdialog --stdout --clear --title "Choose code to type" \
         --menubox "Hi, select your choice. The code will be typed in default browser:" 20 131 4 \
-       "http://bkhome.org/news/" "image" \
-        "http://distro.ibiblio.org/puppylinux/ " "url" \
-       "http://puppylinux.org/wikka/RecentChanges" "quote"  \
-        "http://blog.puppylinux.com/" "wget" \
-        "http://distro.ibiblio.org/quirky/quirky6/amd64/releases/"  "Madafakkar" \
-        "http://woof-ce.26403.n7.nabble.com/"  "bash" \
-        "http://distro.ibiblio.org/fatdog/"  "code" \
-        "http://www.lightofdawn.org/blog/?viewCat=Fatdog64"  "md5sum" `  
+       "http://bkhome.org/news/" "bk" \
+        "http://distro.ibiblio.org/puppylinux/ " "puppy" \
+       "http://puppylinux.org/wikka/RecentChanges" "wikka"  \
+        "http://blog.puppylinux.com/" "blog" \
+        "http://distro.ibiblio.org/quirky/quirky6/amd64/releases/"  "quirky" \
+        "http://woof-ce.26403.n7.nabble.com/"  "woof-ce" \
+        "http://distro.ibiblio.org/fatdog/"  "fatdog" \
+        "http://www.lightofdawn.org/blog/?viewCat=Fatdog64"  "james" `  
         
 
 echo code is $codeselect
