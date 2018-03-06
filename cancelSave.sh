8171721ab35e1b0892f9afe2279866af999bce73
aarf <aarf@yopmail.com>
2018-03-06 19:54:05 +0700

    Add files via upload

---
+++ cancelSave.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#wmpoweroff &
+
+sleep 2
+
+xdotool getactivewindow
+
+xdotool keydown key Tab; sleep 1; xdotool keyup Tab
+xdotool keydown key Tab; sleep 1; xdotool keyup Tab
+
+xdotool click 
+xdotool keydown key KP_Enter; sleep 1; xdotool keyup KP_Enter
