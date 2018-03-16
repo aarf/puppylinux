fde72d2f4a2aa4285c06fcac955a891ccf2602ad
aarf <aarf@yopmail.com>
2018-03-17 06:11:55 +0700

    Add files via upload

--- coinbork.sh
+++ coinbork.sh
@@ -11,4 +11,14 @@ curl  "https://coinmarketcap.com/coins/" --output coin.txt
 sed -n '/Bitcoin/,/BTC/p' < out.txt > out2.txt
 #<a href="/currencies/bitcoin/#markets" class="price" data-usd="8682.32" data-btc="1.0" >$8682.32</
 head -n 14 < out2.txt > out3.txt
-xcowsay < out3.txt
+ #tr "\n" " " < out3.txt > output4.txt
+ tr "\n" "@ " < out3.txt > output4.txt
+ tr -d '[:space:]' < output4.txt > output5.txt
+tr "@" " " < output5.txt > output6.txt
+#tr " " "\n" < output6.txt > output7.txt
+#wget -4c "https://files.coinmarketcap.com/generated/sparklines/1.png"
+wget  -4c https://s2.coinmarketcap.com/generated/sparklines/1.png
+#xcowsay -t 9 --image /root/btcimage.jpeg < output6.txt
+gm composite 1.png /root/black.png  combined.png
+
+xcowsay -t 9 --left --image combined.png < output6.txt
