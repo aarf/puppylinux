8558c2564a892071fe0e7bdf93d0df09e9d88e10
aarf <aarf@yopmail.com>
2018-04-14 18:14:49 +0700

    Add files via upload

---
+++ makedirs4ldd.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir /root/x1/
+mkdir /root/x1/lib
+mkdir /root/x1/lib32
+mkdir /root/x1/lib64
+mkdir /root/x1/usr
+mkdir /root/x1/usr/lib
+mkdir /root/x1/usr/lib64
+mkdir /root/x1/usr/lib32
+
