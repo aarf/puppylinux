3a6cdbceb93b62bc1e076c00b3fd61abbabd30d6
aarf <aarf@yopmail.com>
2018-04-18 11:00:59 +0700

    Add files via upload

--- googlemapsslimopen.sh
+++ googlemapsslimopen.sh
@@ -3,14 +3,16 @@
 #xclip -selection primary -o>>/root/input_text.txt
 select=$( xclip -selection primary -o )
 echo $select
-#input_text=`cat /root/input_text.txt`
+#input_text=cat /root/input_text.txt
 #rm /root/input_text.txt
+slimaddress=$( cat slimaddress.txt )
 
 
-echo "You entered:$input_text"
+echo "You entered:$slimaddress" 
 
-/mnt/sda11/slimjet_amd64-18.0.3.0.tar.xz*extracted/slimjet/spotslim https://www.google.com.kh/maps/place/"$select"
-#sleep 25
+#/mnt/sda11/slimjet_amd64-18.0.4.0.tar.xz*extracted/slimjet/spotslim https://www.google.com/maps/place/"$select"
+ echo $( $slimaddress   https://www.google.com/maps/place/"$select" ) 
+sleep 25
 
 xdotool   keydown key Return; sleep 0.3; xdotool   keyup Return ;
 
